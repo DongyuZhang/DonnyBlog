99363e99afecde2b3ae8fea03f28891222ad72e6
dongyu zhang <dongyuzhang@dongyus-MacBook-Pro.local>
2018-04-18 15:55:22 -0400

    Initial commit

---
+++ src/main/java/com/gwu/donny/DonnyApplication.java
@@ -0,0 +1,12 @@
+package com.gwu.donny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonnyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonnyApplication.class, args);
+    }
+}
